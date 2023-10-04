a79bd4a37c99a8b40f8f55de33a1e557737628cd
Michael Termotto <mjtermotto11@gmail.com>
2023-10-04 21:18:11 +0200

    Added JS functions

--- js/characterMatch.js
+++ js/characterMatch.js
@@ -1,8 +1,34 @@
-exports.isCharacterMatch = function(string1, string2) {
+function isCharacterMatch(string1, string2) {
+	string1 = string1.toLowerCase()
+	string2 = string2.toLowerCase()
 
+	let current_index = 0
+	while (string1.length > 0){
+		let current_len = string1.length;
+		for (let letter of string2){
+			if(string1[0] == letter){
+				string1 = string1.substring(1);
+				current_index += 1
+				break;
+            }
+        }
+		if (current_len == string1.length){
+            return false;
+        }
+    }
+	return true;
 };
 
-exports.anagramsFor = function(word, listOfWords) {
-
+function anagramsFor(checkWord, listOfWords) {
+    anagramArray = [];
+    for (let word of listOfWords){
+        if(isCharacterMatch(checkWord, word)){
+            anagramArray.push(word);
+        }
+    }
+    return anagramArray;
 };
+module.exports = { isCharacterMatch, anagramsFor };
+
+
 
---
+++ js/characterMatch.spec.js
@@ -0,0 +1,45 @@
+const { isCharacterMatch, anagramsFor } = require("./characterMatch.js");
+
+
+
+describe("test isCharacterMatch", () => {
+    test("isCharacterMatch('charm', 'march') === true", () => {
+      expect(isCharacterMatch('charm', 'march')).toBe(true);
+    });
+    test("isCharacterMatch('zach', 'attack') === false", () => {
+        expect(isCharacterMatch('zach', 'attack')).toBe(false);
+    });
+    test("isCharacterMatch('CharM', 'mARcH') === true", () => {
+        expect(isCharacterMatch('CharM', 'mARcH')).toBe(true);
+    });
+    test("isCharacterMatch('abcde2', 'c2abed') === true", () => {
+        expect(isCharacterMatch('abcde2', 'c2abed')).toBe(true);
+    });
+    test("isCharacterMatch('Anna Madrigal', 'A man and a girl') === true", () => {
+        expect(isCharacterMatch('Anna Madrigal', 'A man and a girl')).toBe(true);
+    });
+
+    listOfWords = ["threads", "trashed", "hardest", "hatreds", "hounds"];
+    test("anagramsFor('threads', listOfWords) length == 4", () => {
+        expect(anagramsFor('threads', listOfWords).length).toBe(4);
+    });
+    test("anagramsFor('apple', listOfWords) length == 0", () => {
+        expect(anagramsFor('apple', listOfWords).length).toBe(0);
+    });
+    test("anagramsFor('threads', listOfWords)[0] == 'threads'", () => {
+        expect(anagramsFor('threads', listOfWords)[0]).toBe("threads");
+    });
+    test("anagramsFor('threads', listOfWords)[1] == 'trashed'", () => {
+        expect(anagramsFor('threads', listOfWords)[1]).toBe("trashed");
+    });
+    test("anagramsFor('threads', listOfWords)[2] == 'hardest'", () => {
+        expect(anagramsFor('threads', listOfWords)[2]).toBe("hardest");
+    });
+    test("anagramsFor('threads', listOfWords)[3] == 'hatreds'", () => {
+        expect(anagramsFor('threads', listOfWords)[3]).toBe("hatreds");
+    });
+});
+  
+
+
+
