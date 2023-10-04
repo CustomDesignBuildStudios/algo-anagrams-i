494793623345f224eb47667b7e4072907f299529
Michael Termotto <mjtermotto11@gmail.com>
2023-10-04 20:55:30 +0200

    Finished Python IsAnagram functions

--- python/character_match.py
+++ python/character_match.py
@@ -2,11 +2,26 @@
 
 # Part 1
 def is_character_match(string1, string2):
-	# Your code here
-	pass
+	string1 = string1.lower()
+	string2 = string2.lower()
+
+	current_index = 0
+	while len(string1) > 0:
+		current_len = len(string1)
+		for letter in string2:
+			if string1[0] == letter:
+				string1 = string1[1:]
+				current_index += 1
+				break
+		if current_len == len(string1):
+			return False
+	return True
 
 
 # Part 2
-def anagrams_for(word, list_of_words):
-	# your code here
-	pass
+def anagrams_for(check_word, list_of_words):
+	anagram_array = []
+	for word in list_of_words:
+		if is_character_match(word, check_word):
+			anagram_array.append(word)
+	return anagram_array
